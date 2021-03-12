0764ce26d2a212da23b54535f2d06ac39ee6a74a
NIKHIL K S <linux.nikhil@gmail.com>
2021-03-12 16:53:06 +0530

    Install & configure with let's encrypt

--- http_on_amazon_linux_2.sh
+++ http_on_amazon_linux_2.sh
@@ -12,3 +12,25 @@ ServerSignature Off
 ServerTokens Prod
 EOT
 
+# now install Let's encrypt 
+wget -r --no-parent -A 'epel-release-*.rpm' https://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/
+rpm -Uvh dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/epel-release-*.rpm
+
+# Now configure the apache with new site (replace the domain name)
+cat <<EOT >>  /etc/httpd/conf.d/example.conf
+<VirtualHost *:80>
+    DocumentRoot "/var/www/html"
+    ServerName "example.com"
+    ServerAlias "www.example.com"
+</VirtualHost>
+EOT
+
+systemctl restart httpd
+yum install -y certbot python2-certbot-apache
+
+cat <<EOT>> /etc/crontab
+39      1,13    *       *       *       root    certbot renew --no-self-upgrade
+EOT
+systemctl restart crond
+# Now run certbot and complete the proccess
+certbot
