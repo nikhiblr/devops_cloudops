960b0e47a01f6eadeed6d043841ada3153c6a2d6
NIKHIL K S <linux.nikhil@gmail.com>
2021-03-12 16:41:29 +0530

    Install and harden apache
    After installing the apache a small hardening not going in depth.

--- http_on_amazon_linux_2.sh
+++ http_on_amazon_linux_2.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
 yum install httpd mod_ssl -y
+
+# start the service and enable it 
+systemctl start httpd && systemctl enable httpd
+
+# a little bit of hardening :) not going in depth.
+
+cat <<EOT >> /etc/httpd/conf/httpd.conf
+ServerSignature Off
+ServerTokens Prod
+EOT
+
