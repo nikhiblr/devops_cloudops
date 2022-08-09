93964355e68cc32c452eedf4991ee947db7b9d9c
Nikhil Sukumaran <nikhilsukumaran@nsukumaran.local>
2022-08-09 22:30:06 +0530

    Adding encryption script

--- encryption.sh
+++ encryption.sh
@@ -5,3 +5,5 @@ openssl req -x509 -nodes -days 7300 -newkey rsa:2048 -keyout mysqldump-secure.pr
 echo "output" | openssl smime -encrypt -binary -text -aes256 -out database.sql.enc -outform DER mysqldump-secure.pub.pem
 
 openssl smime -decrypt -in database.sql.enc -binary -inform DEM -inkey mysqldump-secure.priv.pem -out database.sql
+
+mysqldump --user=$MYSQL_USER -p$MYSQL_PASSWORD --lock-tables $db | openssl smime -encrypt -binary -text -aes256 -out "$BACKUP_DIR/$DATE/$db.sql.enc" -outform DER mysqldump-secure.pub.pem
