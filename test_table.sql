bedde9bde041ac262dd0f3526c41f59fcc4a63f0
Nikhil Sukumaran <nikhilsukumaran@nsukumaran.local>
2022-08-09 21:05:00 +0530

    Adding backup scripts

---
+++ test_table.sql
@@ -0,0 +1,4 @@
+CREATE TABLE authors (id INT, name VARCHAR(20), email VARCHAR(20));
+INSERT INTO authors (id,name,email) VALUES(1,"Test","teat@mydomain.com");
+INSERT INTO authors (id,name,email) VALUES(2,"Nk","N@gmail.com");
+INSERT INTO authors (id,name,email) VALUES(3,"Tom","tom@yahoo.com");
