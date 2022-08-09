fc3f83a527726aadd123d9828b37b853b608c107
Nikhil Sukumaran <nikhilsukumaran@nsukumaran.local>
2022-08-09 22:12:19 +0530

    Adding encryption script

--- encryption.sh
+++ encryption.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-openssl req -x509 -nodes -days 7300 -newkey rsa:2048 -keyout mysqldump-secure.priv.pem -out mysqldump-secure.pub.pem -subj "/C=IN/ST=Karnataka/L=Bangalore/O=Ron Company/CN=Nikhil.com"
+openssl req -x509 -nodes -days 7300 -newkey rsa:2048 -keyout mysqldump-secure.priv.pem -out mysqldump-secure.pub.pem -subj "/C=US/ST=New York/L=Brooklyn/O=Creative World/CN=Creative World"
 
 echo "output" | openssl smime -encrypt -binary -text -aes256 -out database.sql.enc -outform DER mysqldump-secure.pub.pem
 
